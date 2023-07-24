cd579945d56c17b0635ad24245665d7ca04ddfce
Nicko <36914404+nickokow@users.noreply.github.com>
2023-07-24 15:30:23 +0500

    Update L3P1.py

--- L3P1.py
+++ L3P1.py
@@ -1,2 +1,5 @@
 opisanye,vozrast,imya = input('Вид питомца: '),input('Возраст: '),input('Кличка: ')
 print(f'{opisanye} по имени \"{imya}\".',f'возраст: {vozrast}')
+#----------------------------------------------------------------------------------#
+a,b,c,d,e,f,g = input(),input(),input(),input(),input(),input(),input()
+(print(a,b,c,d,e,f,g, sep='=>'))
